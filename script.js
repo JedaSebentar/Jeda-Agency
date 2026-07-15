343ffa5dca36e0acfe9f3f224480e99511b0a914
Jeda Sebentar <jedasebentarya@gmail.com>
2026-07-16 02:40:46 +0700

    penambahan navbar background blur

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ if (hero && spot) {
     spot.style.setProperty('--y', y + '%');
   });
 }
+
+window.addEventListener("scroll", function() {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
